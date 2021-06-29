06672d37f39073f1f2306d13b18b78dd6c0405d9
Zaeem <ahmedzaeem32@gmail.com>
2021-06-29 23:28:43 +0500

    final update

--- sarcasm-detector.py
+++ sarcasm-detector.py
@@ -1,3 +1,4 @@
+# libraries to be used
 from PIL import Image
 import matplotlib.pyplot as plt
 from keras.models import load_model
@@ -15,25 +16,31 @@ from keras.models import model_from_yaml
 max_words = 1000
 max_len = 150
 
+# caching the load model function
 @st.cache(suppress_st_warning=True, allow_output_mutation=True)
 def load_RNN_model():
+    # loading the RNN model from yaml file
     yaml_file = open('RNN_model.yaml', 'r')
     loaded_model_yaml = yaml_file.read()
     yaml_file.close()
+    # learning saved weights of RNN
     loaded_model = model_from_yaml(loaded_model_yaml)
     loaded_model.load_weights("RNN_model.h5")
+    # loading tokenizer model
     with open('tokenizer.pickle', 'rb') as handle:
         tokenizer = pickle.load(handle)
     return loaded_model, tokenizer
 
-stopwords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
+# initializing the stepwords from nltk
+stopwords = nltk.corpus.stopwords.words('english')
+# initializing lemmatizer and MinMaxScaler
 lemm = WordNetLemmatizer()
 scaler = MinMaxScaler()
-# Title
+
+# Adding Title to streamlit
 st.markdown("<h1 style = 'color:Gold; Text-align: Center; font-size: 40px;'>Sarcasm Detector</h1>", unsafe_allow_html=True)
 
 img = Image.open("./images/sarcasm.jpg")
-  
 # display image using streamlit
 # width is used to set the width of an image
 st.image(img, width = 700)
@@ -57,6 +64,7 @@ def user_text_processing(user_text):
     scaler.transform(user_seq)
     return user_seq
 
+# user input prediction
 def predict_sarcasm(user_seq):
 #     prediction
     prob = RNN_model.predict(user_seq)
@@ -83,7 +91,6 @@ form = st.form(key='my_form')
 user_text = form.text_input(label='Enter Your Text')
 submit = form.form_submit_button(label= 'Predict Sarcasm' )
 st.write('Press Predict Sarcasm to Predict your text nature')
-
 if submit:
     user_seq = user_text_processing(user_text)
     prediction = predict_sarcasm(user_seq)
